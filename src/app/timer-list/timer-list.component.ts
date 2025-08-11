9ba2d54574ca0061e520e29750a013301de3cbb6
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2025-08-11 15:03:42 +0200

    basic portrait mode for phone

--- src/app/timer-list/timer-list.component.ts
+++ src/app/timer-list/timer-list.component.ts
@@ -3,32 +3,83 @@ import { Component, OnInit } from '@angular/core';
 import { OWITimersService } from '../service/owitimers.service';
 import { Timer } from '../model/timer.model';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TimerUtilsService } from '../service/timer-utils.service';
+import { DeviceDetectorService } from 'ngx-device-detector'; // npm install ngx-device-detector
+import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'timer-list',
   imports: [CommonModule, 
             MatTableModule, 
-            MatIconModule],
+            MatIconModule,
+            MatButtonModule,
+            LayoutModule
+         ],
   templateUrl: './timer-list.component.html',
   styleUrl: './timer-list.component.scss'
 })
 export class TimerListComponent implements OnInit {
-displayedColumns: string[] = ['programme', 'channel', 'repeated', 'duration', 'start', 'end', 'sdate', 'options'];
 
+// WARNING: The order here determines the column order, not the order of the columns in the html!!!!
+
+landscapeColumns: string[] = [
+   'programme', 'sdate', 'start', 'channel', 'duration', 'repeated', 'end', 'options'
+];
+portraitColumns: string[] = [
+   'programme', 'sdate', 'start', 'channel'
+];
+
+displayedColumns: string[] = this.landscapeColumns;
 timers : Timer[] | undefined;   
+landscapeDisplay: boolean = false;
+desktopDisplay: boolean = false;
 
    constructor(private timerService: OWITimersService,
-               public utils: TimerUtilsService
-   )
+      private deviceService: DeviceDetectorService,
+               public utils: TimerUtilsService,
+               private bpObservable: BreakpointObserver
+            )
    {
+      this.landscapeDisplay = this.bpObservable.isMatched('(orientation: landscape)'); 
+      this.desktopDisplay = false; // this.deviceService.isDesktop();  always give true when emulating mobile layout 
+      if(this.desktopDisplay || this.landscapeDisplay) {
+         console.log("TimerListComponent.<init>: set landscape columns: dt:" + this.desktopDisplay + " land:" + this.landscapeDisplay);
+         this.displayedColumns = this.landscapeColumns;   
+      }   
+      else
+      {
+         console.log("TimerListComponent.<init>: set portrait columns");
+         this.displayedColumns = this.portraitColumns;
+      }
    }
 
    ngOnInit() 
    {
       console.log('TimerListComponent.ngOnInit: start');
+      this.bpObservable.observe(['(orientation: portrait)'])
+                        .subscribe(result => {
+                           if(result.matches){
+                              this.landscapeDisplay = false;
+                              if(! this.desktopDisplay)
+                              {
+                                 console.log("TimerListComponent.ngOnInit: set portrait columns");
+                                 this.displayedColumns = this.portraitColumns;
+                              }
+                           }
+                        });
+      
+      this.bpObservable.observe(['(orientation: landscape)'])
+                        .subscribe(result => {
+                        if(result.matches){
+                           this.landscapeDisplay = true;
+                           console.log("TimerListComponent.ngOnInit: set landscape columns");
+                           this.displayedColumns = this.landscapeColumns; 
+                        }
+                        });      
       this.loadTimers();
       console.log("TimerListComponent.ngOnInit: finish");
    }
