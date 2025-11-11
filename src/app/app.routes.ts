b30f620858d9eb2391b09d0b7256f0bcaf4dd277
chris <chris@zombie>
2025-11-11 18:10:53 +0100

    bad titlestrategy implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,44 @@
 // app.routes.ts
-import { Routes } from '@angular/router';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 import { TimerListComponent } from './timer-list/timer-list.component';
 import { TimerEditComponent } from './timer-edit/timer-edit.component';
-import { environment } from '../environments/environment';
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TimerUtilsService } from './service/timer-utils.service';
 export const routes: Routes = [
    {path: 'timerlist', title: "Timers", component: TimerListComponent},
    {path: 'timeredit', title: "Timer Edit", component: TimerEditComponent},
    {path: '', title: "Timers", component: TimerListComponent},
 
 ];
+
+
+@Injectable()
+export class HostnameTitleStrategy extends TitleStrategy
+{
+   constructor(private readonly title: Title,
+   private utils: TimerUtilsService)
+   {
+      super();
+   }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    console.log("HostnameTitleStrategy.updateTitle: route title:" + title);
+    if (title !== undefined) {
+      this.title.setTitle(this.utils.titlePrefix() + " " + title);
+    }
+  }
+}
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: TitleStrategy,
+      useClass: HostnameTitleStrategy
+    }
+  ]
+})
+export class AppRoutingModule {}
